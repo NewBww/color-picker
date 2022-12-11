17c18e276741aa1718fb2b9d071f3c6d057d614d
Bowonwit Anothaisintavee <Bowonwit.A@gmail.com>
2022-12-12 02:09:38 +0700

    optimized

--- main.js
+++ main.js
@@ -6,17 +6,21 @@ Created by WebStorm.
 To change this template use File | Settings | File Templates.
 */
 
-let screenLog = document.querySelector('#screen-log');
+const screenLog = document.querySelector('#screen-log');
 const maxWidth = window.screen.availWidth;
 const maxHeight = window.screen.availHeight;
 let colorCode;
-document.addEventListener('mousemove', logKey);
+
+// Try Modify This
+const MIN_SATURATION = 50;
+const LUMINANCE = 67;
+
 function logKey(e) {
     screenLog.innerText = `X/Y: ${e.clientX}, ${e.clientY}`;
-    colorCode = `hsl(${Math.round((e.clientY/maxHeight)*360)}, ${Math.round(50+(1-e.clientX/maxWidth)*50)}%, 67%)`
+    colorCode = `hsl(${Math.round((e.clientY/maxHeight)*360)}, 
+    ${Math.round(MIN_SATURATION+(1-e.clientX/maxWidth)*(100-MIN_SATURATION))}%, ${LUMINANCE}%)`
     document.body.style.backgroundColor = colorCode
 }
-document.addEventListener('click', alertListener)
-function alertListener(e) {
-    alert(colorCode)
-}
+
+document.addEventListener('mousemove', logKey);
+document.addEventListener('click', () => {alert(colorCode)})
